95d02bef2af27fbb06c099d2a871cb67825b7074
peyo <peteryyoon@gmail.com>
2020-05-26 13:07:40 -0700

    bug fixes

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -53,8 +53,7 @@ const UsersService = {
       id: user.id,
       username: xss(user.username),
       nickname: xss(user.nickname),
-      date_created: moment(new Date(user.date_created))
-        .format()
+      date_created: new Date(user.date_created)
     };
   },
 };
