ecbbd855c7a03a559767e97247e6403044a459fd
peyo <peteryyoon@gmail.com>
2020-08-09 20:34:04 -0700

    refactored auth

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -12,14 +12,14 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
   for (const [key, value] of Object.entries(loginUser))
     if (value == null)
       return res.status(400).json({
-        error: `Missing '${key}' in request body.`,
+        error: { message: `Missing '${key}' in request body.` },
       });
 
   AuthService.getUserWithUserName(req.app.get("db"), loginUser.username)
     .then((dbUser) => {
       if (!dbUser)
         return res.status(400).json({
-          error: "Incorrect username or password.",
+          error: { message: "Incorrect username or password." },
         });
 
       return AuthService.comparePasswords(
@@ -28,7 +28,7 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
       ).then((compareMatch) => {
         if (!compareMatch)
           return res.status(400).json({
-            error: "Incorrect username or password.",
+            error: { message: "Incorrect username or password.", },
           });
 
         const sub = dbUser.username;
