90310269f16fa54c7ebe3319ca331c954855792f
peyo <peteryyoon@gmail.com>
2020-08-10 18:21:42 -0700

    refactoring dtc-service.js

--- src/dtc/dtc-service.js
+++ src/dtc/dtc-service.js
@@ -13,7 +13,7 @@ const DtcService = {
         db.raw(`count(DISTINCT comments) AS number_of_comments`)
       )
       .leftJoin("vinmake", "dtc.vinmake_id", "vinmake.id")
-      .leftJoin("comments AS comm", "dtc.id", "comm.dtc_id")
+      .leftJoin("comments", "dtc.id", "comments.dtc_id")
       .groupBy("dtc.id", "vinmake.id");
   },
 
@@ -47,16 +47,11 @@ const DtcService = {
   },
 
   serializeDtc(dtc) {
-    const { vinmake } = dtc;
     return {
       id: dtc.id,
       dtc: dtc.dtc,
       description: dtc.description,
-      vinmake_id: {
-        id: vinmake.id,
-        make_vin: vinmake.make_vin,
-        short_vin: vinmake.short_vin,
-      },
+      vinmake_id: dtc.vinmake_id,
       number_of_comments: Number(dtc.number_of_comments) || 0,
     };
   },
@@ -71,28 +66,14 @@ const DtcService = {
   },
 
   serializeDtcComment(comment) {
-    const { user, vinmake, dtc } = comment;
     return {
       id: comment.id,
       comment: xss(comment.comment),
       date_created: moment(new Date(comment.date_created)).calendar(),
       date_modified: moment(new Date(comment.date_modified)).calendar() || null,
-      vinmake_id: {
-        id: vinmake.id,
-        make_vin: vinmake.make_vin,
-        short_vin: vinmake.short_vin,
-      },
-      dtc_id: {
-        id: dtc.id,
-        dtc: dtc.dtc,
-        description: dtc.description,
-      },
-      user_id: {
-        id: user.id,
-        username: user.username,
-        nickname: user.nickname,
-        date_created: moment(new Date(user.date_created)).calendar(),
-      },
+      vinmake_id: comment.vinmake_id,
+      dtc_id: comment.dtc_id,
+      user_id: comment.user_id,
     };
   },
 };
